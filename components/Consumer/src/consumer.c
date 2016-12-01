2df5fda9cbeb91697a8477e55007ab42401255bf
Daniel DaCosta <chaosape@chaosape.com>
2016-11-30 18:36:05 -0600

    One message transmitting through a monitor.

--- components/Consumer/src/consumer.c
+++ components/Consumer/src/consumer.c
@@ -1,19 +1,35 @@
 #include <camkes.h>
 #include <stdio.h>
+#include <msgtype.h>
 
 static void qd_handler(void* unused) {
   static int fired = 0;
   printf("consumer:qd_handler: received QueueData event.\n");
+  qd_sem_post();
   if (!fired) {
     fired = 1;
     qd_reg_callback(&qd_handler, NULL);
   }
+  dispatch_sem_post();
 }
 
 void pre_init(void) {
   qd_reg_callback(&qd_handler, NULL);
+  dispatch_sem_wait();
+  qd_sem_wait();
+
 }
 
 int run(void) {
+  while(1) {
+    dispatch_sem_wait();
+    printf("consumer:run: dispatch initiated.\n");
+    if(qd_sem_trywait()==0) {
+      msg_t m;
+      mon_dequeue(&m);
+      printf("consumer:run: dequeued message %u.\n",m.data);
+    }
+    printf("consumer:run: dispatch completed.\n");
+  }
   return 0;
 }
--- components/Producer/src/producer.c
+++ components/Producer/src/producer.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <msgtype.h>
+#include <unistd.h>
 
 void run(void) {
   msg_t m;
-  m.data = 1;
-  printf("producer:run: sending message %u.\n",m.data);  
-  mon_enqueue(&m);
+  m.data = 0;
+//  while(1) {
+    m.data++;
+    printf("producer:run: sending message %u.\n",m.data);  
+    mon_enqueue(&m);
+//    sleep(1);
+//  }
 }
